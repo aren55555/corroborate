0382eb141c282a8201ab34a1e3c498eee825ae4d
Aren Patel <aren55555@users.noreply.github.com>
2015-08-10 01:05:51 -0700

    Putting specs into directories for organization sake.

--- __tests__/functions/is_array.spec.js
+++ __tests__/functions/is_array.spec.js
@@ -1,5 +1,5 @@
-import '../auto_mock_off';
-import {isArray} from '../app/validators/functions/types/is_array';
+import '../../auto_mock_off';
+import {isArray} from '../../app/validators/functions/types/is_array';
 
 describe('isArray', () => {
   it('returns true when Array', () => {
